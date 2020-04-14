5fd19bf739f35fc0d1b1db3a5ae7f68410776685
tritiumm <juebanmonage@163.com>
2020-04-14 13:10:55 +0800

    optimize animation and add some interactions!

--- src/modules/Home/Bot.tsx
+++ src/modules/Home/Bot.tsx
@@ -18,6 +18,7 @@ interface uiItem {
   style: object;
   type?: 'mail';
   clickHandler?: () => void;
+  img: string;
 }
 const Bot = React.forwardRef((props:{
   eventEmit: Function,
@@ -43,13 +44,15 @@ const Bot = React.forwardRef((props:{
       left: DEFAULT_LEFT,
       bottom: DEFAULT_RIGHT
     },
+    img: require('./assets/aboutme.png'),
     clickHandler: () => {
       setIsAboutMeVisible(true);
     }
   },{
     index: 1,
     title: 'talk with me',
-    message: '你想干嘛？',
+    message: '你要跟我聊天吗？',
+    img: require('./assets/talkwithme.png'),
     style: {
       left: DEFAULT_LEFT,
       bottom: DEFAULT_RIGHT
@@ -73,13 +76,15 @@ const Bot = React.forwardRef((props:{
       left: DEFAULT_LEFT,
       bottom: DEFAULT_RIGHT
     },
+    img: require('./assets/zcool.png'),
     clickHandler: () => {
       window.open('https://www.zcool.com.cn/u/642528');
     }
   },{
     index: 4,
     title: 'github',
-    message: '都是黑历史…啊啊啊',
+    message: '其实没啥东西',
+    img: require('./assets/github.png'),
     style: {
       left: DEFAULT_LEFT,
       bottom: DEFAULT_RIGHT
@@ -91,6 +96,7 @@ const Bot = React.forwardRef((props:{
     index: 5,
     title: 'email',
     message: '哦？欢迎给我写信',
+    img: require('./assets/email.png'),
     style: {
       left: DEFAULT_LEFT,
       bottom: DEFAULT_RIGHT
@@ -110,7 +116,7 @@ const Bot = React.forwardRef((props:{
     setIsDialogVisible(false);
   }
   // 获取几个交互ui的位置
-   function getPosition(count: number, r:number, ref: any): Array<number[]> {
+  function getPosition(count: number, r:number, ref: any): Array<number[]> {
     const arr = new Array(count);
     if (ref){
       const botHeight = ref.clientHeight;
@@ -131,14 +137,12 @@ const Bot = React.forwardRef((props:{
         for (let i = midNum; i >0; i--) {
           // let leftOffset = -(r * Math.cos(Math.PI*(midNum -i + 1) * 1/12) - 1/2 * botWidth - BUBBLE_RADIUS);
           let bottomOffset = r * Math.sin(Math.PI * (midNum -i + 1) * 1/6);
-          console.log(midNum, i, Math.cos(1/6 * Math.PI*(midNum -i + 1)))
           let leftOffset = arr[midNum][0] + r * (1 - Math.cos(1/6 * Math.PI * (midNum -i + 1)));
           arr[midNum - (midNum - i + 1)]  = [leftOffset, arr[midNum][1] - bottomOffset]
           arr[midNum + (midNum - i + 1)]  = [leftOffset, arr[midNum][1] + bottomOffset]
         }
       }
     }
-    console.log(arr);
     return arr;
   }
   function controlActionUI(isVisible: boolean, botDom: HTMLElement) {
@@ -168,23 +172,37 @@ const Bot = React.forwardRef((props:{
   }
   /* effect */
   useEffect(()=>{
-    let eye = document.querySelectorAll('.eye');
+    let eyes = document.querySelectorAll('.eye') as NodeListOf<HTMLElement>;
     let container = document.querySelector('body');
-    container && container.addEventListener('mousemove', calculation);
-    function calculation(event: any) {
-      eye.forEach((item: any, index) => {
-        let x = item.offsetLeft + item.clientWidth / 2; // 眼睛的x坐标
-        let y = item.offsetTop + item.clientHeight / 2; // 眼睛的y坐标
-        let rad = Math.atan2(event.pageX - x, event.pageY - y); // 鼠标和眼睛的坐标距离，然后用atan2函数计算出该点与(0, 0)点之间的弧度
-        let rot = (rad * (180 / Math.PI) * -1) + 180; // 转换成角度
-        item.style.cssText = 'transform: rotate(' + rot + 'deg)';
-      })
+
+    // function calculation(event: any) {
+    //   eye.forEach((item: any, index) => {
+    //     let x = item.offsetLeft + item.clientWidth / 2; // 眼睛的x坐标
+    //     let y = item.offsetTop + item.clientHeight / 2; // 眼睛的y坐标
+    //     let rad = Math.atan2(event.pageX - x, event.pageY - y); // 鼠标和眼睛的坐标距离，然后用atan2函数计算出该点与(0, 0)点之间的弧度
+    //     let rot = (rad * (180 / Math.PI) * -1) + 180; // 转换成角度
+    //     item.style.cssText = 'transform: rotate(' + rot + 'deg)';
+    //   })
+    // }
+
+    const calculation = (e: any) => {
+      if (e.target.className && e.target.className.includes && e.target.className.indexOf('eye') > -1) {
+        return;
+      }
+      eyes.forEach(eye => {
+          const x = eye.getBoundingClientRect().left + (eye.clientWidth / 2);
+          const y = eye.getBoundingClientRect().top + (eye.clientHeight / 2);
+          const radian = Math.atan2(e.pageX - x, e.pageY - y);
+          const rot = (radian * (180 / Math.PI) * -1) + 90;
+          eye.style.transform = `rotate(${rot}deg)`;
+      });
     }
+    container && container.addEventListener('mousemove', calculation);
     return () => {
       // 清除订阅
       container && container.removeEventListener('mousemove', calculation);
     };
-  },[])
+  }, []);
   useEffect(()=>{
     if (ref.current && messageRef.current) {
       const botHeight = ref.current.clientHeight;
@@ -230,6 +248,7 @@ const Bot = React.forwardRef((props:{
           <div className="eyes">
             <div className="eye left-eye"></div>
             <div className="eye right-eye"></div>
+         
           </div>
         </div>
         <div className={`message-pop ${isMessageVisle ? 'active' : 'inactive'}`} ref={messageRef}>
@@ -260,7 +279,8 @@ const Bot = React.forwardRef((props:{
                   onClick={item.clickHandler}
                 >
                   <div className="info-text opacity-control">{item.title}</div>
-                  {item.type === 'mail' && <a href="mailto:name@email.com"></a>}
+                  {item.type === 'mail' && <a href="mailto:juebanmonage@163.com"> <img src={item.img} /></a>}
+                  {item.type !== 'mail'&& <img src={item.img} />}
                 </li>
               )
             })
